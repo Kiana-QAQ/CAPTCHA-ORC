d8e7e96a5b4fb70d54574fb2e244ddb1bc6e460e
Kiana-QAQ <z1715378133@outlook.com>
2024-07-10 19:32:46 +0800

    初始提交

---
+++ imageText.py
@@ -0,0 +1,28 @@
+import ddddocr
+import cv2
+
+det = ddddocr.DdddOcr(det=True, ocr=True)
+
+with open("input.jpg", 'rb') as f:
+    image = f.read()
+
+bboxes = det.detection(image)
+# res = det.classification(image)
+print(bboxes)
+
+# 用于保存裁切后的图片
+cropped_images = []
+
+im = cv2.imread("input.jpg")
+
+for i, bbox in enumerate(bboxes):
+    x1, y1, x2, y2 = bbox
+    im = cv2.rectangle(im, (x1, y1), (x2, y2), color=(0, 0, 255), thickness=2)
+    # 裁切图片
+    cropped_image = im[y1:y2, x1:x2]
+    # 保存裁切后的图片
+    cv2.imwrite(f"images/img{i}.jpg", cropped_image)
+    # 将裁切后的图片添加到列表中
+    cropped_images.append(cropped_image)
+
+cv2.imwrite("output.jpg", im)
---
+++ txtText.py
@@ -0,0 +1,24 @@
+# example.py
+import ddddocr
+import os
+
+ocr = ddddocr.DdddOcr()
+def a(x):
+    try:
+        c = os.listdir(x)
+        return len(c)
+    except Exception as e:
+        print(f"Error: {e}")
+        return 0
+
+
+fileName = "images"
+
+fileNum = a(fileName)
+
+print(f"在目录'{fileName}'下有{fileNum}个文件")
+
+for i in range(fileNum):
+    image = open(f"images/img{i}.jpg", "rb").read()
+    result = ocr.classification(image)
+    print(f"第{i + 1}个字为{result}")
